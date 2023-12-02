6a2cf181b598d319caf780a2f439cfee269c44d5
Alina-Luo <llby_luo@outlook.com>
2023-12-02 17:50:12 -0500

    Add documentation

--- lionagi/session/message.py
+++ lionagi/session/message.py
@@ -4,13 +4,42 @@ from ..utils.log_util import DataLogger
 
 
 class Message:
+    """
+    A class modeling messages in conversations.
+
+    This class is designed to encapsulate messages exchanged between different roles
+    (user, assistant, or system) in a conversation. It includes functionality to process,
+    log, and manage messages.
+
+    Attributes:
+        role: The role of the message (user, assistant, or system).
+        content: The content of the message.
+        sender: The sender of the message.
+        logger: An instance of DataLogger for logging message information.
+
+    Methods:
+        __call__: Process and log a message based on its role.
+    """
     def __init__(self) -> None:
+        """
+        Initialize a Message object with attributes for role, content, sender, and a DataLogger.
+        """
         self.role = None
         self.content = None
         self.sender = None
         self.logger = DataLogger()
 
     def __call__(self, system=None, instruction=None, response=None, context=None, sender=None):
+        """
+        Process and log a message based on the specified role (system, instruction, or response).
+
+        Args:
+            system: The content of the message in the system role.
+            instruction: The content of the message in the user role.
+            response: The content of the message in the assistant role.
+            context: Additional context for the user instruction.
+            sender: The sender of the message.
+        """
         
         if sum(map(bool, [system, instruction, response])) > 1:
             raise ValueError("Message cannot have more than one role.")
