d75961b6527244fa2f45ef38252047d5832d1b6f
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2023-12-03 15:14:17 -0500

    more edits, should be a set version now

--- lionagi/utils/__init__.py
+++ lionagi/utils/__init__.py
@@ -1,10 +1,10 @@
-from .sys_util import to_list, str_to_num, make_copy, to_temp, to_csv, hold_call, ahold_call, l_call, al_call, m_call, am_call, e_call, ae_call, get_timestamp
+from .sys_util import to_list, str_to_num, make_copy, to_temp, to_csv, hold_call, ahold_call, l_call, al_call, m_call, am_call, e_call, ae_call, get_timestamp, flatten_dict, flatten_list
 from .api_util import StatusTracker, RateLimiter, BaseAPIService, AsyncQueue
 from .doc_util import dir_to_path, read_text, dir_to_files, chunk_text, file_to_chunks, file_to_chunks, get_bins
 from .log_util import DataLogger
 
 __all__ = [
-    "to_list", "str_to_num", "make_copy", "to_temp", "to_csv", "hold_call", "ahold_call", "l_call", "al_call", "m_call", "am_call", "e_call", "ae_call", "get_timestamp",
+    "to_list", "str_to_num", "make_copy", "to_temp", "to_csv", "hold_call", "ahold_call", "l_call", "al_call", "m_call", "am_call", "e_call", "ae_call", "get_timestamp", "flatten_dict", "flatten_list",
     "StatusTracker", "RateLimiter", "BaseAPIService", "AsyncQueue",
     "dir_to_path", "read_text", "dir_to_files", "chunk_text", "file_to_chunks", "file_to_chunks", "get_bins",
     "DataLogger"
