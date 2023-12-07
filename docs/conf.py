f1fd549d63f9f2fa29325285d8514b2f5426da4f
Alina-Luo <llby_luo@outlook.com>
2023-12-06 21:22:53 -0500

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,30 @@
+import lionagi.version
+
+ver = lionagi.version.__version__
+
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+project = "lionagi"
+copyright = "2023, Haiyang Li"
+author = "Haiyang Li"
+
+version = ver
+release = ver
+
+# -- General configuration
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    "sphinx.ext.coverage",
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+templates_path = ["_templates"]
+
+# -- Options for HTML output
+html_title = project + " " + version
+
+
