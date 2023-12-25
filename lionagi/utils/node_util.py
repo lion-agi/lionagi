b9b631dc194e0ab6c9b915bac524d12b1945fa39
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2023-12-25 11:10:50 -0500

    moved chunker

--- lionagi/utils/node_util.py
+++ lionagi/utils/node_util.py
@@ -1,5 +1,5 @@
 import json
-from typing import Any, Dict, Union
+from typing import Any, Dict, Optional, List, Union
 from .sys_util import create_id
 
 from pydantic import BaseModel, Field
@@ -62,8 +62,35 @@ class BaseNode(BaseModel):
     
     def get_id(self):
         return self.id_
+
     
-    
+class ConditionalRelationship(BaseModel):
+    """
+    Represents a conditional relationship between two nodes in a graph.
+
+    Attributes:
+        target_node_id (str): Identifier of the target node.
+        properties (Dict[str, Any]): Properties associated with the relationship.
+        condition (Optional[str]): Condition that must be satisfied for the relationship to take effect.
+    """
+    label: Optional[str] = None
+    target_node_id: str
+    properties: Dict[str, Any] = Field(default_factory=dict)
+    condition: Optional[str] = None
+
+    def check_condition(self, context: Dict[str, Any]) -> bool:
+        """
+        Check if the condition is satisfied based on the provided context.
+
+        Args:
+            context (Dict[str, Any]): Context to evaluate the condition against.
+
+        Returns:
+            bool: `True` if condition is satisfied, `False` otherwise.
+        """
+        return context.get(self.condition, False)
+
+
 class DataNode(BaseNode):
 
 
@@ -79,3 +106,22 @@ class DataNode(BaseNode):
     
     def to_langchain(self):
         ...
+
+
+class Chunk(DataNode):
+    """
+    Represents a data node.
+
+    Args:
+        BaseNode (lionagi.nodes.base.BaseNode): BaseNode class for data validation.
+    """
+    
+    
+class File(DataNode):
+    """
+    Represents a data node.
+
+    Args:
+        BaseNode (lionagi.nodes.base.BaseNode): BaseNode class for data validation.
+    """
+    pass
