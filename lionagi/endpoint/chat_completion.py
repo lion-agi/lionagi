60dea841ddabcafc71d5ac717264642518186422
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2023-12-27 16:55:44 -0500

    updating

--- lionagi/endpoint/chat_completion.py
+++ lionagi/endpoint/chat_completion.py
@@ -1,79 +1,34 @@
-import asyncio
-import aiohttp
+from .endpoint_utils import call_api, create_payload
 
 
 
-def 
-
-
-
-
-
-
-def _create_payload_chatcompletion(self, schema, **kwargs):
-    """
-    Create a payload for chat completion based on the conversation state and configuration.
-
-    Parameters:
-        kwargs: Additional keyword arguments for configuration.
-
-    Returns:
-        dict: The payload for chat completion.
-    """
-    # currently only openai chat completions are supported
-    messages = self.conversation.messages
-    config = {**self.llmconfig, **kwargs}
-    
-    
-    
-    payload = {
-        "messages": messages,
-        "model": config.get('model'),
-        "frequency_penalty": config.get('frequency_penalty'),
-        "n": config.get('n'),
-        "presence_penalty": config.get('presence_penalty'),
-        "response_format": config.get('response_format'),
-        "temperature": config.get('temperature'),
-        "top_p": config.get('top_p'),
-        }
-    
-    for key in ["seed", "stop", "stream", "tools", "tool_choice", "user", "max_tokens"]:
-        if bool(config[key]) is True and str(config[key]) != "none":
-            payload.update({key: config[key]})
-    return payload
-
-async def _call_chatcompletion(self, sleep=0.1,  **kwargs):
-    """
-    Make a call to the chat completion API and process the response.
-
-    Parameters:
-        sleep (float): The sleep duration after making the API call. Default is 0.1.
-        kwargs: Additional keyword arguments for configuration.
-    """
-    endpoint = f"chat/completions"
+def handle_response(session, payload, completion):
     try:
-        async with aiohttp.ClientSession() as session:
-            payload = self._create_payload_chatcompletion(**kwargs)
-            completion = await self.api_service.call_api(
-                            session, endpoint, payload)
-            if "choices" in completion:
-                self._logger({"input":payload, "output": completion})
-                self.conversation.add_messages(response=completion['choices'][0])
-                self.conversation.responses.append(self.conversation.messages[-1])
-                self.conversation.response_counts += 1
-                await asyncio.sleep(sleep)
-                status_tracker.num_tasks_succeeded += 1
-            else:
-                status_tracker.num_tasks_failed += 1
+        if "choices" in completion:
+            session._logger({"input":payload, "output": completion})
+            session.conversation.add_messages(response=completion['choices'][0])
+            session.conversation.responses.append(session.conversation.messages[-1])
+            session.conversation.response_counts += 1
+            session.status_tracker.num_tasks_succeeded += 1
+        else:
+            session.status_tracker.num_tasks_failed += 1
+                        
     except Exception as e:
-        status_tracker.num_tasks_failed += 1
+        session.status_tracker.num_tasks_failed += 1
         raise e
 
-endpoint, 
-payload = self._create_payload_chatcompletion(**kwargs)
-response = self.call_api()
-return self.process_response(response)
 
 
-async def call():
-    if 
+
+
+    endpoint, 
+    payload = self._create_payload_chatcompletion(**kwargs)
+    response = self.call_api()
+    return self.process_response(response)
+
+
+
+
+
+async def serve(session, ):
+    
---
+++ lionagi/endpoint/endpoint_utils.py
@@ -0,0 +1,40 @@
+import aiohttp
+from ..llm_configs import oai_schema
+
+
+def create_payload(self, schema=oai_schema, **kwargs):
+    """
+    Create a payload for chat completion based on the conversation state and configuration.
+
+    Parameters:
+        kwargs: Additional keyword arguments for configuration.
+
+    Returns:
+        dict: The payload for chat completion.
+    """
+    # currently only openai chat completions are supported
+    messages = self.conversation.messages
+    config = {**self.llmconfig, **kwargs}
+    
+    payload = {"messages": messages}
+    for key in schema['required']:
+        payload.update({key: config[key]})
+
+    for key in schema['optional']:
+        if bool(config[key]) is True and str(config[key]).lower() != "none":
+            payload.update({key: config[key]})
+    return payload
+
+async def call_api(payload, service, endpoint="chat/completions"):
+    """
+    Make a call to the chat completion API and process the response.
+
+    Parameters:
+        sleep (float): The sleep duration after making the API call. Default is 0.1.
+        kwargs: Additional keyword arguments for configuration.
+    """
+
+    async with aiohttp.ClientSession() as session:
+        completion = await service.call_api(session, endpoint, payload)
+        return completion
+    
