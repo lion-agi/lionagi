21b1b5f61eafb45c3a1d2773d5a03fdca2f0bc78
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2023-12-27 16:03:35 -0500

    modified tool manager

--- lionagi/utils/tool_utils.py
+++ lionagi/utils/tool_utils.py
@@ -9,31 +9,31 @@ from ..schema import BaseNode, SimpleTool
 
 class ToolManager(BaseNode):
     registry : Dict = {}
-        
-    def to_dict(name, tool):
-        return {f"{name}": tool}
 
     def _name_existed(self, name):
         return True if name in self.registry.keys() else False
             
-    def _register_function(self, name, tool, parser=None, content=None, update=False, new=False, prefix=None, postfix=None):
+    def _register_tool(self, tool, name=None, update=False, new=False, prefix=None, postfix=None):
+        
         if self._name_existed(name):
             if update and new:
                 raise ValueError(f"Cannot both update and create new registry for existing function {name} at the same time")
-            
-            
-        if len(name) > len(tool.func.__name__):
-            if new and not postfix: 
-                if str_to_num(name, int) is not None:
-            
-        
-        
-        name = f"{prefix or ''}{name}{postfix or i}" if new else name                
-        self.registry.update(self._to_dict(name, func, content, parser)) 
+
+            if len(name) > len(tool.func.__name__):
+                if new and not postfix: 
+                    try:
+                        idx = str_to_num(name[-3:], int)
+                        if idx > 0: 
+                            postfix = idx + 1
+                    except:
+                        pass
+
+        name = f"{prefix or ''}{name}{postfix}" if new else tool.func.__name__                
+        self.registry.update({name:tool}) 
                 
     async def invoke(self, name, *args, **kwargs):
         if self._name_existed(name):
-            func = self.registry[name]["function"]
+            func = self.registry[name]
             try:
                 if asyncio.iscoroutinefunction(func):
                     return await func(*args, **kwargs)
@@ -70,35 +70,6 @@ class ToolManager(BaseNode):
             except:
                 raise ValueError('response is not a valid function call')
     
-    @staticmethod
-    def _from_tool(tool, func):
-        """
-        Convert tool information to function registration parameters.
-
-        Parameters:
-            tool (dict): The tool information.
-            func (callable): The function associated with the tool.
-
-        Returns:
-            Tuple[str, callable, list]: The function name, the function, and the list of function parameters.
-
-        """
-        return (tool['function']['name'], func, 
-                tool['function']['parameters']['properties'].keys())
-        
-    def register_tools(self, tools, funcs, update=False, new=False, prefix=None, postfix=None):
-        """
-        Register multiple tools and their corresponding functions.
-
-        Parameters:
-            tools (list): The list of tool information dictionaries.
-            functions (list): The list of corresponding functions.
-            update (bool): Whether to update existing functions.
-            new (bool): Whether to create new registries for existing functions.
-            prefix (Optional[str]): A prefix to add to the function names.
-            postfix (Optional[str]): A postfix to add to the function names.
-
-        """
-        _f = l_call(range(len(tools)), lambda i: self._from_tool(tools[i], funcs[i]))
-        l_call(range(len(tools)), lambda i: self._register_function(_f[i][0], _f[i][1], update=update, new=new, prefix=prefix, postfix=postfix))
+    def register_tools(self, tools, update=False, new=False, prefix=None, postfix=None ):
+        l_call(tools, self._register_tool, update=update, new=new, prefix=prefix, postfix=postfix)
         
