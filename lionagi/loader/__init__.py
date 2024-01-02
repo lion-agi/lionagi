7899442f975736ffd7ae89184570a19664ed51d4
Alina-Luo <llby_luo@outlook.com>
2024-01-02 16:32:17 -0500

    Update plain reader and chunker

--- lionagi/loader/__init__.py
+++ lionagi/loader/__init__.py
@@ -1,10 +1,12 @@
-from .reader import load, ReaderType
-from .chunker import chunk, datanodes_convert, ChunkerType
+from .reader import load, ReaderType, text_reader
+from .chunker import chunk, datanodes_convert, ChunkerType, text_chunker
 
 __all__ = [
     'load',
     'chunk',
     'datanodes_convert',
+    'text_reader',
+    'text_chunker',
     'ReaderType',
     'ChunkerType'
 ]
