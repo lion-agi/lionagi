b83ab7ddee94efa318557dbc68c51a1c5e170758
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2024-02-24 21:12:11 -0500

    updated requirements

--- lionagi/integrations/bridge/llamaindex_/node_parser.py
+++ lionagi/integrations/bridge/llamaindex_/node_parser.py
@@ -1,9 +1,13 @@
-from llama_index.core.node_parser.interface import NodeParser
+from typing import Any
 from lionagi.util import SysUtil, to_list
+from lionagi.util.import_util import ImportUtil
 
-
-def get_llama_index_node_parser(node_parser: str | NodeParser):
-    if not isinstance(node_parser, [str, NodeParser]):
+def get_llama_index_node_parser(node_parser: Any ):
+    
+    ImportUtil.check_import('llama-index')
+    from llama_index.core.node_parser.interface import NodeParser
+    
+    if not isinstance(node_parser, [str]):
         raise TypeError(
             f'node_parser must be a string or NodeParser, not {type(node_parser)}')
 
@@ -22,12 +26,13 @@ def get_llama_index_node_parser(node_parser: str | NodeParser):
         return node_parser
 
 
-def llama_index_parse_node(documents, node_parser: str | NodeParser,
+def llama_index_parse_node(documents, node_parser: Any,
                            parser_args=None, parser_kwargs=None):
     parser_args = parser_args or []
     parser_kwargs = parser_kwargs or {}
     parser = get_llama_index_node_parser(node_parser)
 
+    ImportUtil.check_import('llama-index')
     from llama_index.core.schema import Document as LlamaDocument
 
     if isinstance(to_list(documents)[0], LlamaDocument):
