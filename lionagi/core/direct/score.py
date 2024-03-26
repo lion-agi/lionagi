6ffb5c1b376e5167544a4254b284ca7d5d4105da
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2024-03-26 11:58:47 -0400

    majority voting

--- lionagi/core/direct/score.py
+++ lionagi/core/direct/score.py
@@ -1,5 +1,5 @@
 from pydantic import Field
-
+import numpy as np
 from lionagi.libs import func_call, convert
 from ..prompt.prompt_template import ScoredTemplate
 from ..branch import Branch
@@ -55,7 +55,7 @@ score context according to the following constraints
             "precision": num_digit if num_digit != 0 else None,
         }
 
-async def score(
+async def _score(
     sentence,
     instruction=None,
     score_range=(1, 10),
@@ -80,6 +80,11 @@ async def score(
     tool_manager=None,
     **kwargs,
 ):
+    
+    if "temperature" not in kwargs:
+        kwargs["temperature"] = 0.1
+
+    instruction = instruction or ""
 
     branch = Branch(
         name=branch_name,
@@ -116,3 +121,73 @@ async def score(
     )
 
     return _template
+
+# async def group_score(sentence, *args, num_instances=5, **kwargs):
+#     sentences = [sentence for _ in range(num_instances)]
+    
+#     outs_ = await func_call.alcall(sentences, _score, *args, **kwargs)
+    
+#     return np.mean([i.answer for i in outs_])
+
+
+async def score(
+    sentence,
+    num_instances=1,
+    instruction=None,
+    score_range=(1, 10),
+    inclusive=True,
+    num_digit=0,
+    confidence_score=False,
+    reason=False,
+    retries=2,
+    delay=0.5,
+    backoff_factor=2,
+    default_value=None,
+    timeout=None,
+    branch_name=None,
+    system=None,
+    messages=None,
+    service=None,
+    sender=None,
+    llmconfig=None,
+    tools=None,
+    datalogger=None,
+    persist_path=None,
+    tool_manager=None,
+    return_template=True,
+    **kwargs,
+):
+    async def _inner(i=0):
+        return await _score(
+            sentence=sentence, 
+            instruction=instruction,
+            score_range=score_range,
+            inclusive=inclusive,
+            num_digit=num_digit,
+            confidence_score=confidence_score,
+            reason=reason,
+            retries=retries,
+            delay=delay,
+            backoff_factor=backoff_factor,
+            default_value=default_value,
+            timeout=timeout,
+            branch_name=branch_name,
+            system=system,
+            messages=messages,
+            service=service,
+            sender=sender,
+            llmconfig=llmconfig,
+            tools=tools,
+            datalogger=datalogger,
+            persist_path=persist_path,
+            tool_manager=tool_manager,
+            **kwargs
+        )
+
+    if num_instances == 1:
+        _out = await _inner()
+        return _out if return_template else _out.answer
+        
+    elif num_instances > 1:
+        _outs = await func_call.alcall(range(num_instances), _inner)
+        return _outs if return_template else np.mean([i.answer for i in _outs])
