36a739ff336bcea220d90f79af0476c2c5c7b421
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2024-04-24 11:25:16 -0400

    added rule, validator

--- lionagi/experimental/validator/validator.py
+++ lionagi/experimental/validator/validator.py
@@ -1,3 +1,4 @@
+from pydantic import BaseModel, Field
 from .rule import DEFAULT_RULES, Rule
 
 
@@ -19,20 +20,37 @@ order_ = [
     "str",
 ]
 
-class BaseValidator:
-    
-    rules: dict[str, Rule] = rules_
-    order: list[str] = order_
-    
+
+class Validator(BaseModel):
+    """
+    rules contain all rules that this validator can apply to data
+    the order determines which rule gets applied in what sequence.
+    notice, if a rule is not present in the orders, it will not be applied.
+    """
+
+    rules: dict[str, Rule] = Field(
+        default=rules_,
+        description="The rules to be used for validation.",
+    )
+
+    order: list[str] = Field(
+        default=order_,
+        description="The order in which the rules should be applied.",
+    )
+
     async def validate(self, value, *args, strict=False, **kwargs):
 
         for i in self.order:
             if i in self.rules:
                 try:
-                    if a := await self.rules[i].validate(value, *args, **kwargs) is not None:
+                    if (
+                        a := await self.rules[i].validate(value, *args, **kwargs)
+                        is not None
+                    ):
                         return a
                 except Exception as e:
                     raise ValueError(f"failed to validate field") from e
         if strict:
             raise ValueError(f"failed to validate field")
+
         return value
