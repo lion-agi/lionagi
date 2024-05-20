6d22552d6302fcdccee08fade064d0f6ed988cad
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2024-05-19 20:05:08 -0400

    updated license

--- lionagi/core/unit/unit_form.py
+++ lionagi/core/unit/unit_form.py
@@ -1,3 +1,19 @@
+"""
+Copyright 2024 HaiyangLi
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 from enum import Enum
 from lionagi.libs.ln_convert import to_str
 from lionagi.core.collections.abc import Field
@@ -15,7 +31,7 @@ class UnitForm(BaseUnitForm):
             "must use provided functions and parameters, DO NOT MAKE UP NAMES!!!"
             "Flag `action_required` as True if filled."
             "When providing parameters, you must follow the provided type and format, "
-            "if the parameter is a number, you should provide a number like 1, 2, or 1.1 if float is allowed."
+            "if the parameter is a number, you should provide a number like 1, 23, or 1.1 if float is allowed."
         )
     )
 
