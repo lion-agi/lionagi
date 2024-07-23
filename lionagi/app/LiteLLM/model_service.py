44fa909391bb34604d26b324205a15eef8a8b273
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2024-07-23 13:04:29 -0400

    added placeholder files to app folders

---
+++ lionagi/app/LiteLLM/model_service.py
@@ -0,0 +1,52 @@
+from lionagi.libs.ln_api import BaseService
+
+allowed_kwargs = [
+    "model",
+    "frequency_penalty",
+    "max_tokens",
+    "n",
+    "presence_penalty",
+    "response_format",
+    "seed",
+    "stop",
+    "stream",
+    "temperature",
+    "top_p",
+    "tools",
+    "tool_choice",
+    "user",
+]
+
+
+class LiteLLMService(BaseService):
+    def __init__(self, model: str = None, **kwargs):
+        super().__init__()
+
+        from lionagi.libs.sys_util import SysUtil
+
+        SysUtil.check_import("litellm")
+
+        from litellm import acompletion
+
+        self.acompletion = acompletion
+        self.model = model
+        self.kwargs = kwargs
+        self.allowed_kwargs = allowed_kwargs
+
+    async def serve_chat(self, messages, **kwargs):
+        payload = {"messages": messages}
+        config = {}
+        for k, v in kwargs.items():
+            if k in self.allowed_kwargs:
+                config[k] = v
+
+        kwargs = {**self.kwargs, **config}
+
+        try:
+            completion = await self.acompletion(
+                model=self.model, messages=messages, **kwargs
+            )
+            return payload, completion
+        except Exception as e:
+            self.status_tracker.num_tasks_failed += 1
+            raise e
