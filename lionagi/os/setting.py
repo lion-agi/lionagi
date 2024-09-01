8388ec01e2c5e5ab9bd003e9d27c8afffcb0bea4
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2024-09-01 11:42:51 -0400

    updated sys_config to setting

---
+++ lionagi/os/setting.py
@@ -0,0 +1,20 @@
+from lion_core.setting import (
+    SchemaModel,
+    BASE_LION_FIELDS,
+    LionIDConfig,
+    TimedFuncCallConfig,
+    RetryConfig,
+    PydanticSerializationConfig,
+    DEFAULT_TIMEZONE,
+)
+
+
+__all__ = [
+    "SchemaModel",
+    "BASE_LION_FIELDS",
+    "LionIDConfig",
+    "TimedFuncCallConfig",
+    "RetryConfig",
+    "PydanticSerializationConfig",
+    "DEFAULT_TIMEZONE",
+]
