730ca04a49e63188d6183cf1905a00b640e7a75f
Huleji Tukura <61782821+kinghendrix10@users.noreply.github.com>
2024-09-26 15:38:04 -0500

    Update __init__.py

--- lionagi/integrations/config/__init__.py
+++ lionagi/integrations/config/__init__.py
@@ -1,4 +1,5 @@
 from .oai_configs import oai_schema
 from .openrouter_configs import openrouter_schema
+from .groq_configs import groq_schema
 
-__all__ = ["oai_schema", "openrouter_schema"]
+__all__ = ["oai_schema", "openrouter_schema", "groq_schema"]
