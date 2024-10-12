262b559af899513ae296497de1a14aa8aac72285
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2024-10-12 13:08:34 -0400

    modified expand annotation to break_down_annotation for recursive

--- lionagi/core/unit/util.py
+++ lionagi/core/unit/util.py
@@ -1,3 +1,8 @@
+import inspect
+from typing import get_args
+
+from pydantic import BaseModel
+
 retry_kwargs = {
     "retries": 0,  # kwargs for rcall, number of retries if failed
     "delay": 0,  # number of seconds to delay before retrying
@@ -74,3 +79,25 @@ async def _direct(
         )
 
     return await directive.direct(form=form, return_form=True, **kwargs)
+
+
+def break_down_annotation(model: type[BaseModel]):
+
+    def _ispydantic_model(x):
+        return inspect.isclass(x) and issubclass(x, BaseModel)
+
+    if not _ispydantic_model(model):
+        return model
+
+    out = {}
+    for k, v in model.__annotations__.items():
+        if _ispydantic_model(v):
+            out[k] = break_down_annotation(v)
+        elif "list" in str(v) and get_args(v):
+            v = get_args(v)[0]
+            if _ispydantic_model(v):
+                v = break_down_annotation(v)
+            out[k] = [v]
+        else:
+            out[k] = v
+    return out
