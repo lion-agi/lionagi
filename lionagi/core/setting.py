be01b59bec9bb9c36085938194fed05bb90e6d88
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2024-10-19 17:53:56 -0400

    updated setting and dependency

--- lionagi/core/setting.py
+++ lionagi/core/setting.py
@@ -1,8 +1,8 @@
-from time import timezone
+from datetime import timezone
 
 from lion_core.setting import LionIDConfig
 
-DEFAULT_TIMEZONE = timezone.UTC
+DEFAULT_TIMEZONE = timezone.utc
 
 DEFAULT_LION_ID_CONFIG = LionIDConfig(
     n=36,
