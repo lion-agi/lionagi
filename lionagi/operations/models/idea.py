b6be57c3ec63e01ec03e91fe4f9afe1f1300d8d6
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2024-10-19 17:02:46 -0400

    removed redundent files

--- lionagi/operations/models/idea.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class IdeaModel(BaseModel):
-    content: str = Field(
-        ...,
-        description="**Provide a concise and clear description of the idea.**",
-    )
--- lionagi/operations/select/utils.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-from inspect import isclass
-
-
-def is_enum(choices):
-    return isclass(choices) and issubclass(choices, Enum)
