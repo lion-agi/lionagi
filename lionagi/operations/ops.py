b4b95e35f0af8cfb5eb358b1418d72fbe289885b
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2025-01-11 23:37:45 -0500

    feat: remove compress module and refactor operations for improved structure

---
+++ lionagi/operations/ops.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2023 - 2024, HaiyangLi <quantocean.li at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from .brainstorm.brainstorm import brainstorm
+from .plan.plan import plan
+from .select.select import select
+
+__all__ = (
+    "brainstorm",
+    "plan",
+    "select",
+    "compress",
+)
