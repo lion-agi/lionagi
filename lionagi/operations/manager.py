58e1b834c5618f261318c2938c138dfe334fa595
OceanLi <ocean@lionagi.ai>
2025-01-17 00:30:00 +0000

    feat: implement OperationManager class for managing operations registration

---
+++ lionagi/operations/manager.py
@@ -0,0 +1,22 @@
+from typing import Callable
+
+from lionagi.protocols._concepts import Manager
+
+"""
+experimental
+"""
+
+
+class OperationManager(Manager):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.registry: dict[str, Callable] = {}
+        self.register_operations(*args, **kwargs)
+
+    def register_operations(self, *args, **kwargs) -> None:
+        operations = {}
+        if args:
+            operations = {i.__name__ for i in args if hasattr(i, "__name__")}
+        operations.update(kwargs)
+        self.registry.update(operations)
