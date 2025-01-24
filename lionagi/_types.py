31f603958cb261b7451067f2fb687f21739c28d3
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2025-01-24 18:59:57 -0500

    chore: update copyright year to 2025 in multiple files

--- lionagi/_types.py
+++ lionagi/_types.py
@@ -1,2 +1,6 @@
+# Copyright (c) 2023 - 2025, HaiyangLi <quantocean.li at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
 from .operatives.types import *
 from .protocols.types import *
--- lionagi/session/prompts.py
+++ lionagi/session/prompts.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2023 - 2025, HaiyangLi <quantocean.li at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
 LION_SYSTEM_MESSAGE = """
 LION_SYSTEM_MESSAGE
 
