eff04f008df2f894106f5d782a6e707d559dff49
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2025-01-24 18:59:49 -0500

    chore: update copyright year to 2025 in multiple files

--- lionagi/libs/token_transform/llmlingua.py
+++ lionagi/libs/token_transform/llmlingua.py
@@ -1,1 +1,5 @@
+# Copyright (c) 2023 - 2025, HaiyangLi <quantocean.li at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
 # TODO
--- lionagi/libs/token_transform/synthlang.py
+++ lionagi/libs/token_transform/synthlang.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2023 - 2025, HaiyangLi <quantocean.li at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
 # synthlang.py
 """
 SynthLang translator and system prompt generator.
