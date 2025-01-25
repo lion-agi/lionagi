8a7532a3c0d56760f92e130ece39dd0864f75e1a
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2025-01-25 16:38:21 -0500

    fix: suppress warnings for deprecated config keys in ChatCompletion endpoint

--- lionagi/service/endpoints/chat_completion.py
+++ lionagi/service/endpoints/chat_completion.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2023 - 2025, HaiyangLi <quantocean.li at gmail dot com>
 #
 # SPDX-License-Identifier: Apache-2.0
-
+import warnings
 from collections.abc import AsyncGenerator
 
 from .base import EndPoint
@@ -14,6 +14,14 @@ CHAT_COMPLETION_CONFIG = {
     "is_invokeable": True,
     "is_streamable": True,
 }
+
+
+warnings.filterwarnings(
+    "ignore",
+    message=".*Valid config keys have changed in V2.*",
+    category=UserWarning,
+    module="pydantic._internal._config",
+)
 
 
 class ChatCompletionEndPoint(EndPoint):
