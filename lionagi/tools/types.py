cbcb4a2ea52119ce4e68c2c109cacf938dd6cca2
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2025-01-28 14:14:43 -0500

    feat: add copyright and license information to tool files

--- lionagi/tools/types.py
+++ lionagi/tools/types.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2023 - 2025, HaiyangLi <quantocean.li at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+
 from .file.reader import ReaderTool
 
 __all__ = ("ReaderTool",)
